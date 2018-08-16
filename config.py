d1024217949688886bf475fcc19960c2f0581206
Eddie <eddiechapman@users.noreply.github.com>
2018-08-16 17:11:03 -0600

    Remove instance config set up because of heroku

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    PERVADE_MEMBER = os.environ.get('PERVADE_MEMBER')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
