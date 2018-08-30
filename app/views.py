2bf9dd80ed7d561d01032c854cc970690b092cf1
Eddie <eddiechapman@users.noreply.github.com>
2018-08-30 17:47:04 -0500

    Add ResetPasswordForm to imports

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
 from datetime import datetime
-from app.forms import LoginForm, RegistrationForm, CodingForm, ResetPasswordRequestForm
+from app.forms import LoginForm, RegistrationForm, CodingForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.email import send_password_reset_email
 from app.models import User, Award
 
