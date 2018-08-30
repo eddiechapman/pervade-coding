8b62b927d57141d192265d26a9e61c259b626046
Eddie <eddiechapman@users.noreply.github.com>
2018-08-30 18:31:22 -0500

    strip whitespace from end of cell

--- db-tools/db-add-linebreaks.py
+++ db-tools/db-add-linebreaks.py
@@ -11,6 +11,7 @@ def validate_abstract(row):
     row[6] = row[6].replace('<br/>', '\n')
     row[6] = row[6].replace('&#8203', '\n')
     row[6] = row[6].lstrip()
+    row[6] = row[6].rstrip()
     return row
 
 
