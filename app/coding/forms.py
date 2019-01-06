c9438aad82cba3c925776e435e1beff4f8c07e6b
eddiechapman <eddie.chapman4@gmail.com>
2019-01-06 14:13:12 -0600

    Remove duplicate fields now that title and abstract are not coded separately. Add comments field.

--- app/coding/forms.py
+++ app/coding/forms.py
@@ -1,30 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, SubmitField, TextAreaField, StringField
+from wtforms import BooleanField, SubmitField, TextAreaField
 
 class CodingForm(FlaskForm):
-    """Groups the coding forms for an award so they can be submitted together.
-    
-    Attributes:
-        title_codes: A form for coding the award's title.
-        abstract_codes: A form for coding the award's abstract.
-        submit: A button for submitting all the form data.
-    """
-
-    # Title fields
-    case_study_ti = BooleanField('Flag for PRIM&R case study')
-    pervasive_data_ti = BooleanField('Pervasive Data')
-    data_science_ti = BooleanField('Data Science')
-    big_data_ti = BooleanField('Big Data')
-    data_synonyms_ti = StringField('Data synonyms',
-                                     description='Please separate values with a semicolon')
-    # Abstract fields
-    case_study_abs = BooleanField('PRIM&R case study')
-    pervasive_data_abs = BooleanField('Pervasive Data')
-    data_science_abs = BooleanField('Data Science')
-    big_data_abs = BooleanField('Big Data')
-    data_synonyms_abs = TextAreaField('Data synonyms',
-                                      description='Please separate values with a semicolon')
+    """The form for recording coding information about an award."""
+    pervasive_data = BooleanField('Pervasive Data')
+    data_science = BooleanField('Data Science')
+    big_data = BooleanField('Big Data')
+    comments = TextAreaField('Comments')
+    data_synonyms = TextAreaField('Data synonyms', description='Please separate values with a semicolon')
     submit = SubmitField('Submit')
-    
+
     def validate(self):
+        """This should have something more useful but it doesn't work otherwise."""
         return True
