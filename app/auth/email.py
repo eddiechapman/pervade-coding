12fa0cb92489f1e6e5c9aaa78fc2d630fd867ff9
eddiechapman <eddie.chapman4@gmail.com>
2019-06-04 11:47:44 -0500

    Password reset "sender" field referenced config variable as list when it was a single email address string.

--- app/auth/email.py
+++ app/auth/email.py
@@ -11,7 +11,7 @@ def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email(
         '[PERVADE Coding] Reset Your Password',
-        sender=current_app.config['ADMINS'][0],
+        sender=current_app.config['ADMINS'],
         recipients=[user.email],
         text_body=render_template('email/reset_password.txt',
                                   user=user,
